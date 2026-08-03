92de016c8337ee4738f87f42754ec679ad582151
willcfyang <willcfyang@outlook.com>
2026-08-03 17:44:40 +0800

    fix: build iOS UniFFI with simulator slice

--- nym-vpn-core/iOS.mk
+++ nym-vpn-core/iOS.mk
@@ -15,13 +15,15 @@ RELEASE_FLAG := --release
 TARGET_DIR := release
 endif
 
-RUST_TRIPLET := aarch64-apple-ios
+# Device + simulator (cargo-swift --platforms ios without --target builds both slices).
+RUST_TRIPLET_DEVICE := aarch64-apple-ios
+RUST_TRIPLET_SIM := aarch64-apple-ios-sim
 
 LIB_CRATE_NAME := nym-vpn-lib-uniffi
 LIB_CRATE_DIR := $(CURDIR)/crates/$(LIB_CRATE_NAME)
-LIBWG_BUILD_DIR := $(CURDIR)/../build/lib/$(RUST_TRIPLET)
+LIBWG_BUILD_DIR := $(CURDIR)/../build/lib/$(RUST_TRIPLET_DEVICE)
+LIBWG_SIM_BUILD_DIR := $(CURDIR)/../build/lib/$(RUST_TRIPLET_SIM)
 
-RUST_LIB_PATH := $(CURDIR)/target/$(RUST_TRIPLET)/$(TARGET_DIR)/libnym_vpn_lib.a
 WIREGUARD_DIR := $(CURDIR)/../wireguard
 
 # todo: consider migrating libwg builds to makefile to avoid rebuilds but for now this should make this makefile aware of changes to go sources
@@ -29,23 +31,26 @@ LIBWG_SOURCES := $(wildcard $(WIREGUARD_DIR)/libwg/*.go) $(wildcard $(WIREGUARD_
 
 .PHONY: build libwg swift-package clean
 
-all: $(LIBWG_BUILD_DIR)/libwg.a swift-package
+all: $(LIBWG_BUILD_DIR)/libwg.a $(LIBWG_SIM_BUILD_DIR)/libwg.a swift-package
 
-build: $(LIBWG_BUILD_DIR)/libwg.a
-	$(ALL_IDEMPOTENT_FLAGS) cargo build --package $(LIB_CRATE_NAME) --target $(RUST_TRIPLET) $(RELEASE_FLAG)
+build: $(LIBWG_BUILD_DIR)/libwg.a $(LIBWG_SIM_BUILD_DIR)/libwg.a
+	$(ALL_IDEMPOTENT_FLAGS) cargo build --package $(LIB_CRATE_NAME) --target $(RUST_TRIPLET_DEVICE) $(RELEASE_FLAG)
+	$(ALL_IDEMPOTENT_FLAGS) cargo build --package $(LIB_CRATE_NAME) --target $(RUST_TRIPLET_SIM) $(RELEASE_FLAG)
 
-swift-package: $(LIBWG_BUILD_DIR)/libwg.a
+# Omit --target so cargo-swift emits device + ios-simulator slices in the xcframework.
+swift-package: $(LIBWG_BUILD_DIR)/libwg.a $(LIBWG_SIM_BUILD_DIR)/libwg.a
 	cd $(LIB_CRATE_DIR); \
-	$(ALL_IDEMPOTENT_FLAGS) cargo swift package --accept-all --platforms ios --name NymVPNLib --target aarch64-apple-ios --xcframework-name NymVPNLibUniffi $(RELEASE_FLAG) ; \
+	$(ALL_IDEMPOTENT_FLAGS) cargo swift package --accept-all --platforms ios --name NymVPNLib --xcframework-name NymVPNLibUniffi $(RELEASE_FLAG) ; \
 	sed -i '' -e '/\.macOS(\.v10_15)/d' "NymVPNLib/Package.swift"
 
-libwg: $(LIBWG_BUILD_DIR)/libwg.a
+libwg: $(LIBWG_BUILD_DIR)/libwg.a $(LIBWG_SIM_BUILD_DIR)/libwg.a
 
-$(LIBWG_BUILD_DIR)/libwg.a: $(LIBWG_SOURCES)
+$(LIBWG_BUILD_DIR)/libwg.a $(LIBWG_SIM_BUILD_DIR)/libwg.a: $(LIBWG_SOURCES)
 	$(WIREGUARD_DIR)/build-wireguard-go.sh --ios
 
 clean:
 	rm -rf $(LIB_CRATE_DIR)/NymVPNLib
 	rm -rf $(LIB_CRATE_DIR)/generated
-	rm -rf $(LIBWG_BUILD_DIR)
-	cargo clean --target $(RUST_TRIPLET)
+	rm -rf $(LIBWG_BUILD_DIR) $(LIBWG_SIM_BUILD_DIR)
+	cargo clean --target $(RUST_TRIPLET_DEVICE)
+	cargo clean --target $(RUST_TRIPLET_SIM)
